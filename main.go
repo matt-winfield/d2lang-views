27ce502c0e4e1d396909b1a77e4494f25ab5a277
Matthew Winfield <matthew.thomas.winfield@gmail.com>
2026-01-15 21:16:49 +0000

    Add basic output folder and reading file content

--- main.go
+++ main.go
@@ -20,4 +20,27 @@ func main() {
 		color.Yellow("Usage: program <source> <destination>")
 		os.Exit(1)
 	}
+
+	content, err := os.ReadFile(args.Source)
+
+	if err != nil {
+		color.Red("ERR: Unable to read source file: %v", err)
+		os.Exit(1)
+	}
+
+	err = ensureDirExists(args.Destination)
+
+	if err != nil {
+		color.Red("ERR: Unable to create destination directory: %v", err)
+		os.Exit(1)
+	}
+
+	color.Green("Done!" + string(content))
+}
+
+func ensureDirExists(path string) error {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return os.MkdirAll(path, os.ModePerm)
+	}
+	return nil
 }
